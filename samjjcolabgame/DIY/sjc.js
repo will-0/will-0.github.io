a63829dd211f340eb181b52350e5e3723b669779
sam-halfpenny <sam0.5pennys@gmail.com>
2021-03-08 17:38:56 +0000

    restart installated

--- samjjcolabgame/DIY/sjc.js
+++ samjjcolabgame/DIY/sjc.js
@@ -1,213 +1,118 @@
-const precourse=[
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12],
-    [12,12]
-]
-numbers=[0,1,2,3,4,5,6,7,8,9]
-alpha100='ABCDEFGHIJ'
-alpha200='KLMNOPQRST'
-alpha300='abcdefghij'
-alpha400='klmnopqrst'
-function dtpc(code,arr){
-    console.log('your inputed code was ' + code)
-    for(i=0;i<code.length;i++){
-        for(j=0;j<10;j++){
-            if(code[i]==numbers[j]){
-                arr.push(j)
+end=0
+function gameloop(timestamp) {
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+    if(!dead){
+        end=0
+        if(roller[0]<course[0].length){
+            ctx.clearRect(0,0,800,600);
+            for(j=0;j<course.length;j++){
+                for(r=0;r<course[j].length;r++){
+                    if(!obs[j][r].kill){
+                        if(obupdate>r*objsize/2){
+                            obs[j][r].draw(ctx)
+                            obs[j][r].update(deltaTime)
+                        }
+                    }
+                }
             }
-            if(code[i]==alpha100[j]){
-                arr.push(j+100)
+            dodger1.update(deltaTime);
+            dodger1.draw(ctx);
+            bullets.forEach(element => {
+                element.update()
+                element.draw(ctx)
+            })
+            lasers.forEach(element => {
+                element.update()
+                element.draw(ctx)
+            })
+            laseroos.forEach(element => {
+                element.update()
+                element.draw(ctx)
+            })
+            laseras.forEach(element => {
+                element.update()
+                element.draw(ctx)
+            })
+            timer = timer + 1
+        }
+        else{
+            console.log('end of game')
+            dead=true
+        }
+    }
+    else{
+        end++
+        console.log('u dead')
+        if(end>5000){
+            kill=true
+        }
+    }
+    if(!kill){
+        requestAnimationFrame(gameloop)
+    }
+    else{
+        con=prompt('gamecode has paused to prevent computer damage do not worry if you want to continue press y',"y")
+        if(con=='y'){
+            kill=false
+            end=0
+            requestAnimationFrame(gameloop)
+        }
+    }
+    obupdate++
+}
+function ob_maker(){
+    for(j=0;j<course.length;j++){
+        for(i=0;i<course[j].length;i++){
+            if(who[j][i]==0){
+                let new_bullet = new ob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
+                obs[j].push(new_bullet)
             }
-            if(code[i]==alpha200[j]){
-                arr.push(j+200)
+            if(who[j][i]==1){
+                let new_bullet = new bob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
+                obs[j].push(new_bullet)
             }
-            if(code[i]==alpha300[j]){
-                arr.push(j+300)
+            if(who[j][i]==2){
+                let new_bullet = new fob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
+                obs[j].push(new_bullet)
             }
-            if(code[i]==alpha400[j]){
-                arr.push(j+400)
+            if(who[j][i]==3){
+                let new_bullet = new yob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
+                obs[j].push(new_bullet)
+            }
+            if(who[j][i]==4){
+                let new_bullet = new dob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
+                obs[j].push(new_bullet)
             }
         }
     }
 }
-const bpvc=prompt('how many blocks per vertical collumn')
-for(r=0;r<bpvc;r++){
-    dtpc(prompt('enter Quinquagesimal(this is a word i promise) code ' + (r+1)),precourse[r])
-}
-window.addEventListener("keydown", function(e) {
-    // space and arrow keys
-    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-        e.preventDefault();
-    }
-}, false);
-let laseroos = []
-let laseras = []
-function checker(arr){
-    counter=0
-    for(i=0;i<arr.length;i++){
-        counter+=arr[i]
-    }
-    if(counter!=1000){
-        alert('ERROR: blpr does not add up to 100%')
-    }
-}
-const GAME_LENGTH=1000
-function random_map(inar){
-    for(j=0;j<GAME_LENGTH;j++){
-        blockp=Math.floor(Math.random() * 1000)
-        if(blockp<=blpr[0]){
-            block=0
-        }
-        if(blockp>blpr[0]){
-            if(blockp<=blpr[0]+blpr[1]){
-                block=1
+function ptc(){
+    for(j=0;j<precourse.length;j++){
+        for(i=0;i<precourse[j].length;i++){
+            if(precourse[j][i]<100){
+                course[j].push(precourse[j][i])
+                who[j].push(0)
             }
-        }
-        if(blockp>blpr[0]+blpr[1]){
-            if(blockp<=blpr[0]+blpr[1]+blpr[2])
-            block=2
-        }
-        if(blockp>blpr[0]+blpr[1]+blpr[2]){
-            if(blockp<=blpr[0]+blpr[1]+blpr[2]+blpr[3])
-            block=3
-        }
-        if(blockp>blpr[0]+blpr[1]+blpr[2]+blpr[3]){
-            if(blockp<=blpr[0]+blpr[1]+blpr[2]+blpr[3]+blpr[4])
-            block=4
-        }
-        rn=Math.floor(Math.random() * 12)+block*12
-        for(i=0;i<5;i++){
-            switch(rn-i*12){
-                case 0:
-                    precourse[inar].push(0+i*100)
-                    break;
-                case 1:
-                    precourse[inar].push(1+i*100)
-                    break;
-                case 2:
-                    precourse[inar].push(2+i*100)
-                    break;
-                case 3:
-                    precourse[inar].push(3+i*100)
-                    break;
-                case 4:
-                    precourse[inar].push(4+i*100)
-                    break;
-                case 5:
-                    precourse[inar].push(5+i*100)
-                    break;
-                case 6:
-                    precourse[inar].push(6+i*100)
-                    break;
-                case 7:
-                    precourse[inar].push(7+i*100)
-                    break;
-                case 8:
-                    precourse[inar].push(8+i*100)
-                    break;
-                case 9:
-                    precourse[inar].push(9+i*100)
-                    break;
-                case 10:
-                    precourse[inar].push(10+i*100)
-                    break;
-                case 11:
-                    precourse[inar].push(11+i*100)
-                    break;
+            if(precourse[j][i]>99 && 199>precourse[j][i]){
+                course[j].push(precourse[j][i]-100)
+                who[j].push(1)
+            }
+            if(precourse[j][i]>199 && 299>precourse[j][i]){
+                course[j].push(precourse[j][i]-200)
+                who[j].push(2)
+            }
+            if(precourse[j][i]>299 && 399>precourse[j][i]){
+                course[j].push(precourse[j][i]-300)
+                who[j].push(3)
+            }
+            if(precourse[j][i]>399 && 499>precourse[j][i]){
+                course[j].push(precourse[j][i]-400)
+                who[j].push(4)
             }
         }
     }
 }
-function diffr(){
-    hr=0
-    for(j=0;j<who.length;j++){
-        for(i=0;i<who[j].length;i++){
-            switch(who[j][i]){
-                case 0:
-                    hr+=2
-                    break;
-                case 1:
-                    hr++
-                    break;
-                case 2:
-                    hr+=10
-                    break;
-                case 3:
-                    hr+=5
-                    break;
-                case 4:
-                    hr+=3
-                    break;
-            }
-        }
-    }
-    hr=hr*bpvc
-    return hr
-}
-let x = 0
-
-console.log(precourse[0].length)
-function ca(){
-    for(i=0;i<16;i++){
-        precourse[0].push(12)
-    }
-}
-ca()
-const GAME_WIDTH=800
-const GAME_HEIGHT=500
-let lasers = []
-let bullets = []
-const course=[
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
-]
-const who=[
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
-]
-obs=[
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
-]
 class laser{
     constructor(gamewidth,gameheight,x,y,fx,fy,fs){
         this.gamewidth = gamewidth
@@ -277,6 +182,15 @@ class Handler{
                 case 40:
                     dodger1.movedown()
                     break
+                case 82:
+                    roller=[0,0,0,0,0,0,0,0,0,0,0,0]
+                    obupdate=0
+                    resurection()
+                    dodger1.position.x=50
+                    dodger1.position.y=225
+                    break
+                case 69:
+                    kill=true
             }
         });
 
@@ -703,6 +617,227 @@ class lasera{
         this.position.x += this.speed.x; 
     }
 }
+function ca(){
+    for(i=0;i<16;i++){
+        precourse[0].push(12)
+    }
+}
+function random_map(inar){
+    for(j=0;j<GAME_LENGTH;j++){
+        blockp=Math.floor(Math.random() * 1000)
+        if(blockp<=blpr[0]){
+            block=0
+        }
+        if(blockp>blpr[0]){
+            if(blockp<=blpr[0]+blpr[1]){
+                block=1
+            }
+        }
+        if(blockp>blpr[0]+blpr[1]){
+            if(blockp<=blpr[0]+blpr[1]+blpr[2])
+            block=2
+        }
+        if(blockp>blpr[0]+blpr[1]+blpr[2]){
+            if(blockp<=blpr[0]+blpr[1]+blpr[2]+blpr[3])
+            block=3
+        }
+        if(blockp>blpr[0]+blpr[1]+blpr[2]+blpr[3]){
+            if(blockp<=blpr[0]+blpr[1]+blpr[2]+blpr[3]+blpr[4])
+            block=4
+        }
+        rn=Math.floor(Math.random() * 12)+block*12
+        for(i=0;i<5;i++){
+            switch(rn-i*12){
+                case 0:
+                    precourse[inar].push(0+i*100)
+                    break;
+                case 1:
+                    precourse[inar].push(1+i*100)
+                    break;
+                case 2:
+                    precourse[inar].push(2+i*100)
+                    break;
+                case 3:
+                    precourse[inar].push(3+i*100)
+                    break;
+                case 4:
+                    precourse[inar].push(4+i*100)
+                    break;
+                case 5:
+                    precourse[inar].push(5+i*100)
+                    break;
+                case 6:
+                    precourse[inar].push(6+i*100)
+                    break;
+                case 7:
+                    precourse[inar].push(7+i*100)
+                    break;
+                case 8:
+                    precourse[inar].push(8+i*100)
+                    break;
+                case 9:
+                    precourse[inar].push(9+i*100)
+                    break;
+                case 10:
+                    precourse[inar].push(10+i*100)
+                    break;
+                case 11:
+                    precourse[inar].push(11+i*100)
+                    break;
+            }
+        }
+    }
+}
+function diffr(){
+    hr=0
+    for(j=0;j<who.length;j++){
+        for(i=0;i<who[j].length;i++){
+            switch(who[j][i]){
+                case 0:
+                    hr+=2
+                    break;
+                case 1:
+                    hr++
+                    break;
+                case 2:
+                    hr+=10
+                    break;
+                case 3:
+                    hr+=5
+                    break;
+                case 4:
+                    hr+=3
+                    break;
+            }
+        }
+    }
+    hr=hr*bpvc
+    return hr
+}
+function checker(arr){
+    counter=0
+    for(i=0;i<arr.length;i++){
+        counter+=arr[i]
+    }
+    if(counter!=1000){
+        alert('ERROR: blpr does not add up to 100%')
+    }
+}
+function dtpc(code,arr){
+    console.log('your inputed code was ' + code)
+    for(i=0;i<code.length;i++){
+        for(j=0;j<10;j++){
+            if(code[i]==numbers[j]){
+                arr.push(j)
+            }
+            if(code[i]==alpha100[j]){
+                arr.push(j+100)
+            }
+            if(code[i]==alpha200[j]){
+                arr.push(j+200)
+            }
+            if(code[i]==alpha300[j]){
+                arr.push(j+300)
+            }
+            if(code[i]==alpha400[j]){
+                arr.push(j+400)
+            }
+        }
+    }
+}
+function resurection(){
+    for(i=0;i<obs.length;i++){
+        for(j=0;j<obs[i].length;j++){
+            obs[i][j].kill=false
+            obs[i][j].position.x=800
+        }
+    }
+    dead=false
+}
+//actual game code
+
+
+let precourse=[
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12],
+    [12,12]
+]
+numbers=[0,1,2,3,4,5,6,7,8,9]
+alpha100='ABCDEFGHIJ'
+alpha200='KLMNOPQRST'
+alpha300='abcdefghij'
+alpha400='klmnopqrst'
+let bpvc=parseInt(document.getElementById("output").textContent)
+for(r=0;r<bpvc;r++){
+    dtpc(document.getElementById(r).value,precourse[r])
+}
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+let laseroos = []
+let laseras = []
+let GAME_LENGTH=1000
+let x = 0
+ca()
+let GAME_WIDTH=800
+let GAME_HEIGHT=500
+let lasers = []
+let bullets = []
+let course=[
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+let who=[
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+obs=[
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+let kill=false
 let score=0
 let objsize = 50
 let test=false
@@ -714,110 +849,8 @@ let dead=false
 let obupdate=0
 let canvas = document.getElementById("gamescreen")
 let ctx = canvas.getContext('2d')
-function ptc(){
-    for(j=0;j<precourse.length;j++){
-        for(i=0;i<precourse[j].length;i++){
-            if(precourse[j][i]<100){
-                course[j].push(precourse[j][i])
-                who[j].push(0)
-            }
-            if(precourse[j][i]>99 && 199>precourse[j][i]){
-                course[j].push(precourse[j][i]-100)
-                who[j].push(1)
-            }
-            if(precourse[j][i]>199 && 299>precourse[j][i]){
-                course[j].push(precourse[j][i]-200)
-                who[j].push(2)
-            }
-            if(precourse[j][i]>299 && 399>precourse[j][i]){
-                course[j].push(precourse[j][i]-300)
-                who[j].push(3)
-            }
-            if(precourse[j][i]>399 && 499>precourse[j][i]){
-                course[j].push(precourse[j][i]-400)
-                who[j].push(4)
-            }
-        }
-    }
-}
-
 ptc()
-function ob_maker(){
-    for(j=0;j<course.length;j++){
-        for(i=0;i<course[j].length;i++){
-            if(who[j][i]==0){
-                let new_bullet = new ob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
-                obs[j].push(new_bullet)
-            }
-            if(who[j][i]==1){
-                let new_bullet = new bob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
-                obs[j].push(new_bullet)
-            }
-            if(who[j][i]==2){
-                let new_bullet = new fob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
-                obs[j].push(new_bullet)
-            }
-            if(who[j][i]==3){
-                let new_bullet = new yob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
-                obs[j].push(new_bullet)
-            }
-            if(who[j][i]==4){
-                let new_bullet = new dob(GAME_WIDTH,GAME_HEIGHT,objsize,course[j][i])
-                obs[j].push(new_bullet)
-            }
-        }
-    }
-}
 ob_maker()
 console.log('the difficulty rating of this level is ',diffr())
-
 let lastTime = 0
-
-function gameloop(timestamp) {
-    let deltaTime = timestamp - lastTime;
-    lastTime = timestamp;
-    ctx.clearRect(0,0,800,600);
-    for(j=0;j<course.length;j++){
-        for(r=0;r<course[j].length;r++){
-            if(!obs[j][r].kill){
-                if(obupdate>r*objsize/2){
-                    obs[j][r].draw(ctx)
-                    obs[j][r].update(deltaTime)
-                }
-            }
-        }
-    }
-    dodger1.update(deltaTime);
-    dodger1.draw(ctx);
-    bullets.forEach(element => {
-        element.update()
-        element.draw(ctx)
-    })
-    lasers.forEach(element => {
-        element.update()
-        element.draw(ctx)
-    })
-    laseroos.forEach(element => {
-        element.update()
-        element.draw(ctx)
-    })
-    laseras.forEach(element => {
-        element.update()
-        element.draw(ctx)
-    })
-    timer = timer + 1
-    if(!dead){
-        if(roller[0]<course[0].length){
-            requestAnimationFrame(gameloop)
-        }
-        else{
-            console.log('level complete')
-            console.log('your sore is ',score)
-        }
-    }
-    else{
-        console.log('u dead')
-    }
-    obupdate++
-}
 gameloop()
